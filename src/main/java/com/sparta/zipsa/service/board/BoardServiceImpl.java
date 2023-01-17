2b0f942914eb67fc081983e08051908ac963f71d
jusung kim <karlema8900@gmail.com>
2023-01-17 20:14:10 +0900

    feat : 문자열합치기 concat사용

--- src/main/java/com/sparta/zipsa/service/board/BoardServiceImpl.java
+++ src/main/java/com/sparta/zipsa/service/board/BoardServiceImpl.java
@@ -50,7 +50,7 @@ public class BoardServiceImpl implements BoardService {
 
         String deletePrint = Long.toString(boardId);
         String print = "게시글이 삭제되었습니다.";
-        deletePrint = deletePrint + print;
+        deletePrint.concat(print);
         return deletePrint;
     }
 }
