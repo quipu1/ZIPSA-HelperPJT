a8b18a4ea9c8b5a6be7ac98a04a4ffe36d21c038
Rubi <quipu1@naver.com>
2023-01-18 19:35:29 +0900

    Fix: 관리자 기능 예외처리 방법 수정

--- src/main/java/com/sparta/zipsa/exception/status/StatusCode.java
+++ src/main/java/com/sparta/zipsa/exception/status/StatusCode.java
@@ -37,10 +37,13 @@ public enum StatusCode {
     //HELPER
     HELPER(BAD_REQUEST, "HELPER 신청 관련 오류입니다."),
     ALREADY_APPLY_HELPER(BAD_REQUEST, "이미 HELPER 권한을 신청한 상태입니다."),
+    ALREADY_HELPER(BAD_REQUEST, "이미 HELPER 권한인 사용자입니다."),
+    ALREADY_CUSTOMER(BAD_REQUEST, "이미 CUSTOMER 권한인 사용자입니다."),
 
     //ADMIN
     ADMIN(BAD_REQUEST, "ADMIN 관련 오류입니다."),
     ADMIN_PASSWORD_NOT_MATCH(BAD_REQUEST, "관리자 암호가 일치하지 않아 등록이 불가합니다."),
+    INVALID_AUTHORITY(BAD_REQUEST, "존재하지 않는 권한입니다."),
 
     //SERVER
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 에러입니다.");
