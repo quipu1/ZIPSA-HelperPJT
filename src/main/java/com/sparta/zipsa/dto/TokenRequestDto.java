7d55c176c57e1471680adb98034494136581cdec
sung <zoo5607@naver.com>
2023-01-18 14:02:27 +0900

    Feat : jwt / security 추가

---
+++ src/main/java/com/sparta/zipsa/dto/TokenRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.zipsa.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenRequestDto {
+    private String accessToken;
+    private String refreshToken;
+}
