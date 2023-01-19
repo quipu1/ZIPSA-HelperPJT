6ce04d8f4889fb6b374f52e36751ae96b746090e
taewoong Kim <ktw2172@gmail.com>
2023-01-19 12:06:54 +0900

    Feat: 준혁님 로그아웃 기능, User,HelperBoard 관련 리팩토링 및 메소드 수정, 주석 추가

--- src/main/java/com/sparta/zipsa/controller/UserController.java
+++ src/main/java/com/sparta/zipsa/controller/UserController.java
@@ -3,10 +3,12 @@ package com.sparta.zipsa.controller;
 import com.sparta.zipsa.entity.User;
 import com.sparta.zipsa.jwt.JwtUtil;
 import com.sparta.zipsa.security.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.sparta.zipsa.dto.*;
 import com.sparta.zipsa.entity.Board;
 import com.sparta.zipsa.service.user.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -38,6 +40,16 @@ public class UserController {
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(loginDto.getUsername(), loginDto.getRole()));
         return new ResponseEntity("로그인 되었습니다.", HttpStatus.OK);
     }
+    @GetMapping("/logout")
+    public ResponseEntity logout(HttpServletRequest request){
+        // 정보를 컨트롤러로 보냈을 때 HttpServletRequest 객체 안에 모든 데이터가 들어가게 된다.
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // 등록된 세션을 삭제한다.
+        }
+        return new ResponseEntity("로그아웃 되었습니다.", HttpStatus.OK);
+    }
     //프로필 조회
     @GetMapping("/{userId}")
     public ProfileResponseDto getProfile(@PathVariable Long userId) {
@@ -59,4 +71,5 @@ public class UserController {
     public ResponseEntity delete(@RequestBody DeleteRequestDto deleteRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return userService.delete(deleteRequestDto, userDetails.getUser());
     }
+
 }
