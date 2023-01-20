71127fd75522a3cfb673856fbda28fbb14aaf3c1
Rubi <quipu1@naver.com>
2023-01-20 15:13:08 +0900

    Fix: admin 기능 return 타입 수정

--- src/main/java/com/sparta/zipsa/controller/AdminController.java
+++ src/main/java/com/sparta/zipsa/controller/AdminController.java
@@ -1,11 +1,14 @@
 package com.sparta.zipsa.controller;
 
+import com.sparta.zipsa.dto.HelperBoardResponseDto;
+import com.sparta.zipsa.dto.UserResponseDto;
 import com.sparta.zipsa.entity.HelperBoard;
 import com.sparta.zipsa.entity.User;
 import com.sparta.zipsa.service.admin.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +25,7 @@ public class AdminController {
     //요청된 Role에 따라 해당 Role의 사용자 전체 조회
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
-    public Page<User> getUserAllByRoll(
+    public Page<UserResponseDto> getUserAllByRoll(
             @RequestParam("page") int page,
             @RequestParam("size") int size,
             @RequestParam("isAsc") boolean isAsc,
@@ -34,21 +37,21 @@ public class AdminController {
     //helper 신청글 조회
     @GetMapping("/helperboard")
     @ResponseStatus(HttpStatus.OK)
-    public List<HelperBoard> getHelperBoard() {
+    public List<HelperBoardResponseDto> getHelperBoard() {
         return adminService.getHelperBoard();
     }
 
     //helper 권한 등록 요청 승인
     @PutMapping("/authority/accept/{userId}")
     @ResponseStatus(HttpStatus.OK)
-    public void acceptHelperAuthority(@PathVariable Long userId) {
-        adminService.acceptHelperAuthority(userId);
+    public ResponseEntity acceptHelperAuthority(@PathVariable Long userId) {
+        return adminService.acceptHelperAuthority(userId);
     }
 
-    @PutMapping("/authorty/remove/{userId}")
+    @PutMapping("/authority/remove/{userId}")
     @ResponseStatus(HttpStatus.OK)
-    public void removeHelperAuthority(@PathVariable Long userId) {
-        adminService.removeHelperAuthority(userId);
+    public ResponseEntity removeHelperAuthority(@PathVariable Long userId) {
+        return adminService.removeHelperAuthority(userId);
     }
 
 }
