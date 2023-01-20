b7ac85d2eb1f7fad3e276c105ffe927f03601394
taewoong Kim <ktw2172@gmail.com>
2023-01-20 15:20:53 +0900

    Feat: HelperBoard조회기능 추가

--- src/main/java/com/sparta/zipsa/controller/HelperBoardController.java
+++ src/main/java/com/sparta/zipsa/controller/HelperBoardController.java
@@ -1,6 +1,7 @@
 package com.sparta.zipsa.controller;
 
 import com.sparta.zipsa.dto.HelperBoardRequestDto;
+import com.sparta.zipsa.dto.HelperBoardResponseDto;
 import com.sparta.zipsa.security.UserDetailsImpl;
 import com.sparta.zipsa.service.helperBoard.HelperBoardService;
 import lombok.RequiredArgsConstructor;
@@ -10,23 +11,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class HelperBoardController {
 
     private final HelperBoardService helperBoardService;
 
     //집사 신청글 작성
-    @PostMapping("/api/helperBoard")
+    @PostMapping("helperBoard")
     public ResponseEntity createHelperBoard(@RequestBody HelperBoardRequestDto helperBoardRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return helperBoardService.createHelperBoard(helperBoardRequestDto, userDetails.getUser());
     }
+
     //집사 신청글 삭제
-    @DeleteMapping("/api/helperBoard/{helperBoardId}")
+    @DeleteMapping("helperBoard/{helperBoardId}")
     public ResponseEntity deleteHelperBoard(@PathVariable Long helperBoardId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return helperBoardService.deleteHelperBoard(helperBoardId, userDetails.getUser());
     }
+
     //집사 신청글 수정
-    @PutMapping("/api/helperBoard/{helperBoardId}")
+    @PutMapping("helperBoard/{helperBoardId}")
     public ResponseEntity updateHelperBoard(@PathVariable Long helperBoardId, @RequestBody HelperBoardRequestDto helperBoardRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return helperBoardService.updateHelperBoard(helperBoardId, helperBoardRequestDto, userDetails.getUser());
     }
+
+    //집사 신청글 조회
+    @GetMapping("helperBoard/{helperBoardId}")
+    public HelperBoardResponseDto getHelperBoard(@PathVariable Long helperBoardId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return helperBoardService.getHelperBoard(helperBoardId, userDetails.getUser());
+    }
+
 }
